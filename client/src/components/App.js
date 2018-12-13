8b5909ac86c1c0cd11765903d340ed3299f14759
Voltis <v.agolli@live.com>
2018-12-13 21:40:04 +0100

    app navbar

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,12 +5,14 @@ import StreamEdit from './streams/StreamEdit'
 import StreamDelete from './streams/StreamDelete'
 import StreamList from './streams/StreamList'
 import StreamShow from './streams/StreamShow'
+import Header from './Header';
 
 const App = () => {
     return (
-        <div>
+        <div className="ui container">
             <BrowserRouter>
                 <div>
+                    <Header />
                     <Route path="/" exact component={StreamList} />
                     <Route path="/streams/new" exact component={StreamCreate} />
                     <Route path="/streams/edit" exact component={StreamEdit} />
