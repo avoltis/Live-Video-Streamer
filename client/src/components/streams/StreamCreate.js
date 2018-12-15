9ef9c23da2e90416298c043bc53b3ded61e5fbb7
Voltis <v.agolli@live.com>
2018-12-15 13:29:51 +0100

    api server using json server

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -12,6 +12,7 @@ class StreamCreate extends React.Component {
             )
         }
     }
+    
     renderInput = ({ input, label, meta }) => {
         const className = `field ${meta.error && meta.touched ? 'error' : ''}`
 
