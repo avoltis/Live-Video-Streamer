45a7e35359c59403e3999b96292f15b65ed8e193
Voltis <v.agolli@live.com>
2018-12-15 17:47:13 +0100

    stream list call

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchStreams } from '../../actions';
+
+class StreamList extends React.Component {
+  componentDidMount() {
+      this.props.fetchStreams();
+  }
+
+    render() {
+        return <div>StreamList</div>;
+    }
+}
 
 
-const StreamList = () => {
-    return <div>StreamList</div>;
-};
-
-export default StreamList;
+export default connect(null, { fetchStreams })(StreamList);
